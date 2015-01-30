f28343ca0e2282a1405455245f7784139deed1ff
Liam <liamfruzyna@gmail.com>
2015-01-29 18:51:43 -0600

    Added a limit switch to the lift as a place holder

--- src/org/wildstang/subsystems/Lift.java
+++ src/org/wildstang/subsystems/Lift.java
@@ -1,14 +1,23 @@
 package org.wildstang.subsystems;
 
+import org.wildstang.inputmanager.base.InputManager;
 import org.wildstang.inputmanager.inputs.joystick.JoystickAxisEnum;
 import org.wildstang.outputmanager.base.OutputManager;
+import org.wildstang.subjects.base.BooleanSubject;
+import org.wildstang.subjects.base.IObserver;
+import org.wildstang.subjects.base.Subject;
 import org.wildstang.subsystems.base.Subsystem;
 
-public class Lift extends Subsystem
+public class Lift extends Subsystem implements IObserver
 {	
+	boolean atBottom;
+	
 	public Lift(String name)
 	{
 		super(name);
+		atBottom = false;
+		//temp limit switch will probably be hall effect sensor
+		registerForSensorNotification(InputManager.LIFT_BOTTOM_LIMIT_SWITCH_INDEX);
 	}
 	
 	public void init()
@@ -20,8 +29,25 @@ public class Lift extends Subsystem
 	public void update()
 	{
 		double speed = ((Double) (getJoystickValue(false, JoystickAxisEnum.MANIPULATOR_DPAD_Y))).doubleValue();
-		getOutput(OutputManager.WINCH_INDEX).set(new Double(speed));
+		if(!atBottom)
+		{
+			getOutput(OutputManager.WINCH_INDEX).set(new Double(speed));
+		}
+		else
+		{
+			getOutput(OutputManager.WINCH_INDEX).set(new Double(0.0));
+		}
 		getOutput(OutputManager.WINCH_INDEX).update();
+		
+	}
+
+	@Override
+	public void acceptNotification(Subject subjectThatCaused)
+	{
+		if(subjectThatCaused.equals(InputManager.getInstance().getSensorInput(InputManager.LIFT_BOTTOM_LIMIT_SWITCH_INDEX).getSubject()))
+		{
+            atBottom = ((BooleanSubject) subjectThatCaused).getValue();
+		}
 	}
 
 }
