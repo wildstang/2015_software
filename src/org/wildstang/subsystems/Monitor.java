02b216886a745b9f143cbbdf942ef531462b5d8e
Liam <liamfruzyna@gmail.com>
2015-01-31 14:31:15 -0600

    A few fixes

--- src/org/wildstang/subsystems/Monitor.java
+++ src/org/wildstang/subsystems/Monitor.java
@@ -6,38 +6,38 @@ import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Monitor extends Subsystem {
+public class Monitor extends Subsystem
+{
 	
-	AnalogInput input;
-	PowerDistributionPanel pdp = new PowerDistributionPanel();
+	PowerDistributionPanel pdp;
 	/**
 	 * @author Noah
 	 *  Allows the code to use the pdp.getCurrent and 
 	 *  the input.getAverageVoltage command.
 	 */
 	
-	public Monitor(String name) {
+	public Monitor(String name)
+	{
 		super(name);
 	}
 
-	public void init () {
-		
+	public void init ()
+	{
+		pdp = new PowerDistributionPanel();
 	}
 	
-	public void update () {
-		for (int i = 0; i < 16; i = i + 1) {
-			this.input = new AnalogInput(i);
-			// A copy of this command in the WsAnalofInput
-			
+	public void update ()
+	{
+		/*for (int i = 0; i < 16; i = i + 1)
+		{
 			double current = pdp.getCurrent(i);
-			//SmartDashboard.putDouble("PDP output #" + i, current);
-			
-		}
+			SmartDashboard.putDouble("PDP output #" + i, current);
+		}*/
 		
 		double totalCurrent = pdp.getTotalCurrent();
 		SmartDashboard.putDouble("Total Current", totalCurrent);
 		
-		double voltage = input.getVoltage();
+		double voltage = pdp.getVoltage();
 		SmartDashboard.putDouble("Average Voltage", voltage);
 		
 		double pdpTemp = pdp.getTemperature();
