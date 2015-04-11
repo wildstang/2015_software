1a303297d7a36ced0fdf1a4b8ca5281c95e2e8d0
Alex Guerra <aguerra7002@stu.d214.org>
2015-04-11 13:56:24 -0500

    tweaks to bin grabbers auto

--- src/org/wildstang/autonomous/programs/AutonomousProgramBinGrabber.java
+++ src/org/wildstang/autonomous/programs/AutonomousProgramBinGrabber.java
@@ -6,15 +6,19 @@ import org.wildstang.autonomous.steps.bingrabber.AutonomousStepReleaseBinGrabber
 import org.wildstang.autonomous.steps.bingrabber.AutonomousStepRetractBinGrabbers;
 import org.wildstang.autonomous.steps.control.AutonomousStepDelay;
 import org.wildstang.autonomous.steps.drivebase.AutonomousStepDriveManual;
+import org.wildstang.autonomous.steps.drivebase.AutonomousStepSetShifter;
 import org.wildstang.config.DoubleConfigFileParameter;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class AutonomousProgramBinGrabber extends AutonomousProgram{
 	
-	protected final DoubleConfigFileParameter BIN_SPEED = new DoubleConfigFileParameter(this.getClass().getName(), "Stealing_Bin_Speed", 0.75);
+	protected final DoubleConfigFileParameter BIN_SPEED = new DoubleConfigFileParameter(this.getClass().getName(), "Stealing_Bin_Speed", 1.0);
 
 	@Override
 	protected void defineSteps() {
 		// TODO Auto-generated method stub
+		addStep(new AutonomousStepSetShifter(DoubleSolenoid.Value.kReverse));
 		addStep(new AutonomousStepReleaseBinGrabbers());
 		addStep(new AutonomousStepDelay(1000));
 		addStep(new AutonomousStepDriveManual(BIN_SPEED.getValue(), 0)); 
